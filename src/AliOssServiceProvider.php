320ebc018ea382e02e11a6b417f212a5e11a7668
Mark Padilla <mark.padilla@maximum.com>
2018-09-06 14:48:12 +0800

    force large files to be uploaded in the local and added to queued event for uploading to oss

--- src/AliOssServiceProvider.php
+++ src/AliOssServiceProvider.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
 use OSS\OssClient;
+use Illuminate\Support\Facades\Event;
 
 class AliOssServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,7 @@ class AliOssServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Event::listen('Jacobcyl\AliOSS\Events\FileUpload', 'Jacobcyl\AliOSS\Listeners\OssFileUpload');
         //发布配置文件
         /*
         if (function_exists('config_path')) {
@@ -31,22 +33,22 @@ class AliOssServiceProvider extends ServiceProvider
 
         Storage::extend('oss', function($app, $config)
         {
-            $accessId  = $config['access_id'];
-            $accessKey = $config['access_key'];
-            $endPoint  = $config['endpoint']; // 默认作为外部节点
-            $epInternal= empty($config['endpoint_internal']) ? $endPoint : $config['endpoint_internal']; // 内部节点
-            $cdnDomain = empty($config['cdnDomain']) ? '' : $config['cdnDomain'];
-            $bucket    = $config['bucket'];
-            $ssl       = empty($config['ssl']) ? false : $config['ssl']; 
-            $isCname   = empty($config['isCName']) ? false : $config['isCName'];
-            $debug     = empty($config['debug']) ? false : $config['debug'];
-            
+            $accessId       = $config['access_id'];
+            $accessKey      = $config['access_key'];
+            $endPoint       = $config['endpoint']; // 默认作为外部节点
+            $epInternal     = empty($config['endpoint_internal']) ? $endPoint : $config['endpoint_internal']; // 内部节点
+            $cdnDomain      = empty($config['cdnDomain']) ? '' : $config['cdnDomain'];
+            $bucket         = $config['bucket'];
+            $ssl            = empty($config['ssl']) ? false : $config['ssl'];
+            $isCname        = empty($config['isCName']) ? false : $config['isCName'];
+            $debug          = empty($config['debug']) ? false : $config['debug'];
+            $maxFileSize    = empty($config['maxFileSize']) ? 6000000 : $config['maxFileSize'];
+
             if($debug) Log::debug('OSS config:', $config);
 
             $client  = new OssClient($accessId, $accessKey, $epInternal, $isCname);
-            $adapter = new AliOssAdapter($client, $bucket, $endPoint, $ssl, $isCname, $debug, $cdnDomain);
-
-            //Log::debug($client);
+            $adapter = new AliOssAdapter($client, $bucket, $endPoint, $ssl, $isCname, $debug, $cdnDomain, $maxFileSize);
+            
             $filesystem =  new Filesystem($adapter);
             
             $filesystem->addPlugin(new PutFile());
---
+++ src/Events/FileUpload.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jacobcyl\AliOSS\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class FileUpload
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $file;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($file)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
